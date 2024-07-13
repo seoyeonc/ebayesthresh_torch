9c21878af59d40fb149d9031370305bb281fb8e3
seoyeonc <chltjdus1212@gmail.com>
2024-07-13 17:59:52 +0900

    .

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup
 setup(
     name='ebayesthresh_torch',
     version='0.0.1',
-    description='ebayesthresh pip install',
+    description='ebayesthresh_torch pip install',
     url='https://github.com/seoyeonc/ebayesthresh_torch.git',
     author='seoyeonchoi',
     author_email='chltjdus1212@gmail.com',
     license='seoyeonc',
-    packages=['ebayesthresh'],
+    packages=['ebayesthresh_torch'],
     zip_safe=False,
     install_requires=[
         'torch==2.0.1',
